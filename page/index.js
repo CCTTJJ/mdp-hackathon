f96900f5f2bbbcf8ebc393c945ee068571a4b551
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2018-09-18 23:35:41 +0800

    update

--- page/index.js
+++ page/index.js
@@ -1,33 +1,33 @@
 new Vue({
     el: '#app',
     data: {
-        key:"ssd",
-        imgSrc:"",
-        isShow:false,
-        result:"",
-        loading:false,
-        init:true,
+        key: "ssd",
+        imgSrc: "",
+        isShow: false,
+        result: "",
+        loading: false,
+        init: true,
         upload_file: null
         // file:""
     },
-    methods:{
-        upload(){
+    methods: {
+        upload() {
             var self = this;
             this.loading = true;
             this.init = false;
             var file = document.getElementById("file");
             var upload_file = file.files[0],
-            formdata = new FormData(),
-            xhr = new XMLHttpRequest();
-        
-            formdata.append('date',new Date().toLocaleString());  
+                formdata = new FormData(),
+                xhr = new XMLHttpRequest();
+
+            formdata.append('date', new Date().toLocaleString());
             // 将文件添加到formdata对象中，（注：下面的file字段名在node中有用）
             formdata.append('file', upload_file);
             xhr.open("POST", "/upload", true);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
-                    var res = JSON.parse(xhr.responseText)
-                    console.log(res)
+                    var res = JSON.parse(xhr.responseText);
+                    console.log(res);
                     self.imgSrc = res.imgUrl;
                     self.result = res.animalRes.keyword;
                     self.isShow = true;
@@ -37,26 +37,26 @@ new Vue({
                 }
             };
             xhr.send(formdata);
-        
+
         },
-        clear(){
+        clear() {
             this.isShow = false;
             this.loading = false;
             this.init = true
         },
-        allowDrag(e){
+        allowDrag(e) {
             e.preventDefault();
         },
-        drop(e){
+        drop(e) {
             e.preventDefault();
             var self = this;
             this.loading = true;
             this.init = false;
             var upload_file = e.dataTransfer.files[0],
-            formdata = new FormData(),
-            xhr = new XMLHttpRequest();
-        
-            formdata.append('date',new Date().toLocaleString());  
+                formdata = new FormData(),
+                xhr = new XMLHttpRequest();
+
+            formdata.append('date', new Date().toLocaleString());
             // 将文件添加到formdata对象中，（注：下面的file字段名在node中有用）
             formdata.append('file', upload_file);
             xhr.open("POST", "/upload", true);
@@ -76,4 +76,4 @@ new Vue({
         }
 
     }
-  });
+});
